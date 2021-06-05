9ef6f40b3a14c9f0a00b22884e9ba841c56352b6
Jakub Mozgawa <jakub.mozgawa@gmail.com>
2021-06-05 16:38:57 +0200

    [N] Any VP in a state is enough to build spy network in that state

--- common/defines/hip_defines.lua
+++ common/defines/hip_defines.lua
@@ -134,6 +134,8 @@ NDefines.NOperatives.AGENCY_CREATION_DAYS = 30 --30						-- Number of days neede
 NDefines.NOperatives.AGENCY_UPGRADE_DAYS = 75 --30						-- Number of days needed to upgrade an intelligence agency
 
 NDefines.NOperatives.COUNTER_INTELLIGENCE_STACKING_FACTOR = 1.0		-- was 0.5				-- Multiplier applied to each operative after the first one. So if we have the following counter intelligence rating values [ 0.1, 0.3, 0.2 ], the factor is applied twice for the lowest value and once for the 2nd lowest one as such : [ 0.3, 0.2 * D, 0.1 * D * D ] and then the result is summed up to give the final rating value
+NDefines.NOperatives.INTEL_NETWORK_MIN_VP_TO_TARGET = 1					-- was 15 -- The minimum value of the highest VP in a state to consider the state as a valid target to start building an intel network
+
 -- Production Stuff
 
 NDefines.NProduction.MAX_EQUIPMENT_RESOURCES_NEED = 4 	-- Max number of different strategic resources an equipment can be dependent on.
