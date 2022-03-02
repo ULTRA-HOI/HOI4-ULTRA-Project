5b17fc357375121f4e75c228ad7305ac8e33a8d7
Vorondil <reisoreoht@hotmail.com>
2022-03-02 21:37:07 +0100

    corrected non working define + reduced length of dropped supplies to 5 days

--- common/defines/hip_defines.lua
+++ common/defines/hip_defines.lua
@@ -601,7 +601,8 @@ NDefines.NAir.NAVAL_STRIKE_DETECTION_BALANCE_FACTOR = 0.5 --0.7		-- Value used t
 
 NDefines.NAir.AA_INDUSTRY_AIR_DAMAGE_FACTOR = -0.12				-- 5x levels = 60% defense from bombing
 
-NDefines.NAir.AIR_SUPPLY_CONVERSION_SCALE = 0.001	--0.05				-- Conversion scale for planes to air supply
+NDefines.NCountry.AIR_SUPPLY_CONVERSION_SCALE = 0.005	--0.05				-- Conversion scale for planes to air supply
+NDefines.NCountry.AIR_SUPPLY_DROP_EXPIRATION_HOURS = 120  --168            -- Air drop length after being dropped
 
 -- logistic strikes
 
