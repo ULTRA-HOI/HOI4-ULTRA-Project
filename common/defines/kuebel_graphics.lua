84131f10c72dee8f4a98577d80d152f85611c216
Jakub Mozgawa <jmozgawa@egnyte.com>
2022-09-06 08:17:35 +0200

    Small graphical tweaks

--- common/defines/kuebel_graphics.lua
+++
@@ -1,2 +0,0 @@
-NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {400, 900, 1500}
---NDefines_Graphics.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {1000, 1100, 1500} --for modding only
