6632a1255e4426ac22ac5d34edadc9d8814fe05a
Vorondil59 <30769639+Vorondil59@users.noreply.github.com>
2023-04-02 17:39:40 +0200

    Revert "increased AI base reluctance to diplomacy"
    This reverts commit 0f5544ddd6519c4c90a2228b7599f9af038ff17e.

--- common/defines/hip_defines.lua
+++ common/defines/hip_defines.lua
@@ -890,7 +890,7 @@ NDefines.NAI.LENDLEASE_FRACTION_OF_STOCKPILE = 0.5 --0.25
 --NDefines.NAI.DIPLOMACY_SEND_ATTACHE_COST_FACTOR = 1.0	-- Desire to send attache substracts the cost multiplied by this
 --NDefines.NAI.DIPLOMACY_SEND_ATTACHE_PP_FACTOR = 1.0	-- Desire to send attache adds total PP multiplied by this
 
-NDefines.NAI.BASE_RELUCTANCE = 30 --was 20 -- Base reluctance applied to all diplomatic offers
+NDefines.NAI.BASE_RELUCTANCE = 20 -- Base reluctance applied to all diplomatic offers
 
 --------------------------------------------------------------------------------------------------------------
 -- ECONOMY
