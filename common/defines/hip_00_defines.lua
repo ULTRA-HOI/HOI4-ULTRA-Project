09bacf878a6dc0815c973a28ccfd0e551ce303e4
T3rm1dor <104867351+T3rm1dor@users.noreply.github.com>
2023-09-21 14:49:58 +0200

    Air defines change

--- common/defines/hip_00_defines.lua
+++ common/defines/hip_00_defines.lua
@@ -1167,21 +1167,21 @@ NMilitary = {
 
 
 NAir = {
-	AIR_WING_FLIGHT_SPEED_MULT = 0.02, --was 0.02 --Global speed multiplier for airplanes (affects fe.transferring to another base)
+	AIR_WING_FLIGHT_SPEED_MULT = 0.15, --was 0.02 --Global speed multiplier for airplanes (affects fe.transferring to another base)
 	AIR_WING_MAX_STATS_ATTACK = 400, --was 100 --Max stats
 	AIR_WING_MAX_STATS_DEFENCE = 400, --was 100
-	AIR_WING_MAX_STATS_AGILITY = 150, --was 100
-	AIR_WING_MAX_STATS_SPEED = 1500, --was 800
+	AIR_WING_MAX_STATS_AGILITY = 1500, --was 100
+	AIR_WING_MAX_STATS_SPEED = 200, --was 800
 	AIR_WING_MAX_STATS_BOMBING = 150, --was 100
 	AIR_WING_MAX_SIZE = 1600, --was 1000 --Max amount of airplanes in wing
 	AIR_WING_AVERAGE_SIZE = 72, --was 100 --Eyeballed average amount of airplanes in wing. Used when calculating air volunteer.
 	AIR_WING_BOMB_DAMAGE_FACTOR = 1.0, --was 2 --Used to balance the damage done while bombing.
-	BIGGEST_AGILITY_FACTOR_DIFF = 4.0,					-- biggest factor difference in agility for doing damage (caps to this)
-	BIGGEST_SPEED_FACTOR_DIFF = 2.5, --was 3.5 --biggest factor difference in speed for doing damage (caps to this)
+	BIGGEST_AGILITY_FACTOR_DIFF = 1.4,					-- biggest factor difference in agility for doing damage (caps to this)
+	BIGGEST_SPEED_FACTOR_DIFF = 1.2, --was 3.5 --biggest factor difference in speed for doing damage (caps to this)
 	TOP_SPEED_DAMAGE_BONUS_FACTOR = 0, --was 0.025 --A factor for scaling the top speed of a plane into damage buff. If an attacking wing has a speed advantage of any form their speed value will be converted into a percentage bonus with this modifier
 	COMBAT_DAMAGE_STATS_MULTILPIER = 1, --was 0.2
-	COMBAT_BETTER_AGILITY_DAMAGE_REDUCTION = 0, --was 0.30 --How much the better agility (than opponent's) can reduce their damage to us.
-	COMBAT_BETTER_SPEED_DAMAGE_INCREASE = 1, --was 0.60 --How much the better Speed (than opponent's) can reduce increase our damage to them.
+	COMBAT_BETTER_AGILITY_DAMAGE_REDUCTION = 1.25, --was 0.30 --How much the better agility (than opponent's) can reduce their damage to us.
+	COMBAT_BETTER_SPEED_DAMAGE_INCREASE = 2, --was 0.60 --How much the better Speed (than opponent's) can reduce increase our damage to them.
 														-- Both of these defines are combined with their sister FACTOR_DIFF defines to create defense or offensive buffs
 														-- In both cases the maximum bonus or reduction is (BIGGEST_X_FACTOR_DIFF - 1) * COMBAT_BETTER_X_DAMAGE_Y * Damage
 	COMBAT_MAX_WINGS_AT_ONCE = 10000, 						-- Max amount of air wings in one combat simulation. The higher value, the quicker countries may loose their wings. It's a gameplay balance value.
@@ -1296,8 +1296,8 @@ NAir = {
 
 	DISRUPTION_FACTOR = 0.05, --was 4.0 --multiplier on disruption damage to scale its effects on planes
 	DISRUPTION_FACTOR_CARRIER = 1.00, --was 6.0 --multiplier on disruption damage to scale its effects on carrier vs carrier planes
-	DISRUPTION_SPEED_FACTOR = 1.0, --was 1.0
-	DISRUPTION_AGILITY_FACTOR = 0.0, --was 0.0
+	DISRUPTION_SPEED_FACTOR = 0.0, --was 1.0
+	DISRUPTION_AGILITY_FACTOR = 0.125, --was 0.0
 	DISRUPTION_ATTACK_FACTOR = 1.0, --was 0.0
 	DISRUPTION_DETECTION_FACTOR = 1.0, --was 1.0
 	ESCORT_FACTOR = 2.0, --was 2.0
