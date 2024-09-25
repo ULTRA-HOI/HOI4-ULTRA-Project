ca2eb623e5fdae543dc81a3497fd2310858d3aa3
Jakub Mozgawa <jakub.mozgawa@gmail.com>
2024-09-25 15:42:53 +0200

    [Fixes] Standardise names of infantry equipment templates and archetypes

---
+++ localisation/english/Infantry Equipment Templates.rb
@@ -0,0 +1,138 @@
+Japan Heavy Equipment
+
+Battalion Support 1936
+heavy_inf_bat_hmg  heavy_inf_bat_mortar_light
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig
+
+Battalion Support 1937
+heavy_inf_bat_hmg heavy_inf_bat_mortar_light heavy_inf_bat_mortar_heavy
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig heavy_inf_reg_at_1
+
+Battalion Support 1939
+heavy_inf_bat_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_heavy_at_rifle heavy_inf_bat_sniper
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig heavy_inf_reg_at_1
+
+Battalion Support 1941
+heavy_inf_bat_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_heavy_at_rifle heavy_inf_bat_sniper
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig heavy_inf_reg_at_2
+
+Battalion Support 1943
+heavy_inf_bat_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_heavy_at_rifle heavy_inf_bat_sniper
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig heavy_inf_reg_at_2
+
+Battalion Support 1945
+heavy_inf_bat_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_heavy_at_0 heavy_inf_bat_sniper
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig heavy_inf_reg_at_3
+
+
+
+
+
+
+British Heavy Support
+
+Battalion Support 1936
+heavy_inf_bat_hmg heavy_inf_bat_mortar_light heavy_inf_bat_mortar_medium
+
+Battalion Support 1938
+heavy_inf_bat_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_light heavy_inf_bat_mortar_medium heavy_inf_bat_at_rifle
+heavy_inf_reg_at_1 heavy_inf_reg_aa_1
+
+Battalion Support 1943
+heavy_inf_bat_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_engineer heavy_inf_bat_heavy_at_0 heavy_inf_bat_sniper
+heavy_inf_reg_at_2 heavy_inf_reg_aa_2
+
+Battalion Support 1946
+heavy_inf_bat_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_engineer heavy_inf_bat_heavy_at_1 heavy_inf_bat_sniper
+heavy_inf_reg_at_3 heavy_inf_reg_aa_3
+
+Battalion Support 1948
+heavy_inf_bat_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_engineer heavy_inf_bat_heavy_at_1 heavy_inf_bat_sniper
+heavy_inf_reg_at_rc heavy_inf_reg_aa_3
+
+Battalion Support 1950
+heavy_inf_bat_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_engineer heavy_inf_bat_heavy_at_1 heavy_inf_bat_sniper
+heavy_inf_reg_at_atgm heavy_inf_reg_aa_3 
+
+
+
+
+
+
+Italy Heavy Equipment
+
+Battalion Support 1936
+heavy_inf_bat_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_light
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig
+
+Battalion Support 1941
+heavy_inf_bat_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_light heavy_inf_bat_at_rifle_impr
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig
+
+Battalion Support 1943
+heavy_inf_bat_impr_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_light heavy_inf_bat_heavy_at_rifle
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig heavy_inf_reg_at_2, heavy_inf_reg_aa_2 
+
+
+
+
+
+US Heavy Support
+
+Battalion Support 1936
+heavy_inf_bat_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium
+heavy_inf_reg_light_ig
+
+Battalion Support 1940
+heavy_inf_bat_impr_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_light heavy_inf_bat_mortar_medium
+heavy_inf_reg_light_ig heavy_inf_reg_at_1
+
+Battalion Support 1942
+heavy_inf_bat_impr_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_light heavy_inf_bat_mortar_medium heavy_inf_bat_engineer
+heavy_inf_reg_light_ig heavy_inf_reg_at_1
+
+Battalion Support 1943
+heavy_inf_bat_impr_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_engineer heavy_inf_bat_heavy_at_0
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig heavy_inf_reg_at_2
+
+Battalion Support 1944
+heavy_inf_bat_impr_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_engineer heavy_inf_bat_heavy_at_0 heavy_inf_bat_sniper
+heavy_inf_reg_light_ig heavy_inf_reg_heavy_ig heavy_inf_reg_at_2
+
+Battalion Support 1945
+heavy_inf_bat_impr_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_engineer heavy_inf_bat_heavy_at_0 heavy_inf_bat_sniper
+heavy_inf_reg_light_ig_rc heavy_inf_reg_at_rc
+
+Battalion Support 1948
+heavy_inf_bat_impr_hmg heavy_inf_bat_h_hmg heavy_inf_bat_mortar_medium heavy_inf_bat_mortar_heavy heavy_inf_bat_engineer heavy_inf_bat_heavy_at_0 heavy_inf_bat_sniper
+heavy_inf_reg_light_ig_rc heavy_inf_reg_at_rc
+
+
+
+French Squad:
+
+Rifle Squad 1936
+10x Bolt-Action Rifle (including SL & NCO), 1x LMG, Radio 1, Many Grenades
+
+Rifle Squad 1940
+10x Improved Bolt-Action Rifle (including SL & NCO), 1x LMG, Radio 1, Many Grenades
+
+Rifle Squad 1944
+3x Cheap Improved SMG (including SL & NCO), 4x Improved Semi-Auto Rifle, 1x Improved LMG, Radio 3, Light Anti-Tank, Several Grenades
+
+
+
+
+Japan Squads
+
+Rifle Squad 1936
+1x Officer Sword (SL), 11x Bolt-Action Rifle (including NCO), 1x LMG, Radio 1, Many Grenades
+
+Rifle Squad 1941
+1x Officer Sword (SL), 10x Improved Bolt-Action Rifle (including NCO), 1x Improved Bolt-Action Marksman, 1x Improved LMG, Radio 1, Many Grenades
+
+Rifle Squad 1943
+1x Officer Sword (SL), 1x Improved SMG (NCO), 9x Improved Bolt-Action Rifle (including NCO), 1x Improved Bolt-Action Marksman, 1x Improved LMG, Radio 1, Light Anti-Tank, Some Grenades
+
+Rifle Squad 1945
+1x Improved SMG (SL), 1x Cheap SMG (NCO), 4x Semi-Automatic Rifle, 5x Improved Bolt-Action Rifle, 1x Improved Bolt-Action Marksman, 1x Improved LMG, Radio 1, Light Anti-Tank, Some Grenades
