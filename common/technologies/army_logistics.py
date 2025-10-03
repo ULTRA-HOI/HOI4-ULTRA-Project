0c6a6256b0d88cef061ebd35e1647d80bdfb9fb5
Gizid <lukek8000@gmail.com>
2025-10-04 01:25:55 +1000

    [NC] reworked implementation of base battalion supply

--- common/technologies/army_logistics.py
+++ common/technologies/army_logistics.py
@@ -1,4 +1,5 @@
 # number of technologies for each logistics category
+# = 5 * tech_granularity_mult as defined in scripted_effects/army_logisitics.txt#calc_motorisation_techs
 num_techologies = 50
 
 # enter all the off-map logistics categories
@@ -35,7 +36,7 @@ logi_modifiers = [
 # for battalions that have an extra base multiplicative supply modifier
 # format: ["UNIT/CATEGORY", "LOGI_CATEGORY", base_supply
 # "UNIT/CATEGORY": unit / category name of what has extra supply, make sure its prepended with category_ if a category
-# "LOGI_CATEGORY": what logistics category this unit/category belongs to IMPORTANT: this category should not be defined in the unit file
+# "LOGI_CATEGORY": what logistics category this unit/category belongs to. Both of these categories should be defined in the unit file
 # base_supply: extra multiplicative supply modifier inherent to the category/unit. i.e. 0.2 = 20% extra supply. This cannot be negative
 outlier_battalions = [
     ["category_mot_log_extra_supply", "mot", 0.15],
@@ -56,11 +57,10 @@ for category in logi_categories:
             if outlier[1] == category[0]:
                 output_string += f"{outlier[0]} = {{ "
                 for modifier in logi_modifiers[category[2]]:
-                    value = modifier[1] + (modifier[2] - modifier[1]) * (i / num_techologies)
-                    value += value * (category[1] - 1) * modifier[3]
                     if modifier[0] == "supply_consumption":
-                        value = (1 + value) * (1 + outlier[2]) - 1
-                    output_string += f"{modifier[0]} = {value:.3f} "
+                        value = modifier[1] + (modifier[2] - modifier[1]) * (i / num_techologies)
+                        value = (1 + value) * (1 + outlier[2]) - 1 - value
+                        output_string += f"{modifier[0]} = {value:.3f} "
                 output_string += "} "
         output_string += "}\n"
     output_string += "\n"
