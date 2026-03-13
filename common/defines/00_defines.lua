689bb2e99632516ddd811f20a5f8d4bdacd5298a
Gizid <lukek8000@gmail.com>
2026-03-14 04:28:47 +1100

    [Balance] Decreased AA damage

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -977,7 +977,7 @@ NMilitary = {
 	ENEMY_AIR_SUPERIORITY_DEFENSE_STEEPNESS = 120, --was 112 --how quickly defense approaches the max impact diminishing returns curve
 	ENEMY_AIR_SUPERIORITY_SPEED_IMPACT = -0.4, --was -0.3 --effect on speed due to enemy air superiority
 
-	ANTI_AIR_TARGETTING_TO_CHANCE = 0.045, --was 0.07 --Balancing value to determine the chance of ground AA hitting an attacking airplane, affecting both the effective average damage done by AA to airplanes, and the reduction of damage done by airplanes due to AA support
+	ANTI_AIR_TARGETTING_TO_CHANCE = 0.04, --was 0.07 --Balancing value to determine the chance of ground AA hitting an attacking airplane, affecting both the effective average damage done by AA to airplanes, and the reduction of damage done by airplanes due to AA support
 	ANTI_AIR_ATTACK_TO_AMOUNT = 0.002, --was 0.005 --Balancing value to convert equipment stat anti_air_attack to the random % value of airplanes being hit.
 
 	ENCIRCLED_PENALTY = 0, --was -0.3 --penalty when completely encircled
